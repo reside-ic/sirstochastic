753a045ebe61a4530f7db242fede87309233f451
Carol Brickley <c.brickley@ic.uk.ac>
2020-10-01 17:08:56 +0100

    Initial commit

--- R/display.R
+++ R/display.R
@@ -1,10 +1,24 @@
-# time <- seq(0, 100, by = 0.01)
-# newtime <- time(1:10000)
-# newxS <- xS[1:10000]
-# newyI <- yI[1:10000]
-# newzR <- zR[1:10000]
-#
-# sir_col <- c("#8c8cd9", "#cc0044", "#999966")
-# par(mar = c(4.1, 5.1, 0.5, 0.5), las = 1)
-# matplot(newtime, cbind(newxS, newyI, newzR), type = "l", col = sir_col, lty = 1)
-# legend("topright", lwd = 1, col = sir_col, legend = c("S", "I", "R"), bty = "n")
+
+#' Displays S, I and R over time
+#'
+#' @param xS
+#' @param yI
+#' @param zR
+#' @param dt
+#'
+#' @return
+#' @export
+#'
+#' @examples
+display <- function(xS, yI, zR, dt) {
+  time <- seq(0, 100, by = 0.01)
+  newtime <- time(1:10000)
+  newxS <- xS[1:10000]
+  newyI <- yI[1:10000]
+  newzR <- zR[1:10000]
+
+  sir_col <- c("#8c8cd9", "#cc0044", "#999966")
+  par(mar = c(4.1, 5.1, 0.5, 0.5), las = 1)
+  matplot(newtime, cbind(newxS, newyI, newzR), type = "l", col = sir_col, lty = 1)
+  legend("topright", lwd = 1, col = sir_col, legend = c("S", "I", "R"), bty = "n")
+}
